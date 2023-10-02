4e760c6242c45cf20a06ce896bf66b0d55a7c47c
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-02 13:34:27 -0400

    first commit

---
+++ Unsolved/controllers/api/userRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    // TODO: Add a comment describing the functionality of this expression
+    //this finds the user in the database that matches what was typed in on the login page
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this expression
+    //this is checking if the inputted password matches the one in the database. returns a boolean 
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this method
+    //
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // TODO: Add a comment describing the functionality of this method
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
